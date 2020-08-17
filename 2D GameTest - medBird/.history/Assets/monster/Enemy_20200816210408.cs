c06a1165655fbb107a4627f85bd5fd4012c57bda
Yarin Michael Bouzaglo <62481484+YarinBou@users.noreply.github.com>
2020-08-17 15:41:19 +0300

    Add project

---
+++ 2D GameTest - medBird/.history/Assets/monster/Enemy_20200816210408.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if (bird != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 2D GameTest - medBird/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2D_GameTest___medBird
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
